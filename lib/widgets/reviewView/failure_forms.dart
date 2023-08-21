0f04d2d26e9d77293f8ec481e186ba6fb7bc1324
temevh <temehama@gmail.com>
2023-08-21 09:27:00 +0300

    added failure dropdown and likedtask checkbox

--- lib/widgets/reviewView/failure_forms.dart
+++ lib/widgets/reviewView/failure_forms.dart
@@ -8,16 +8,19 @@ class FailureForm extends StatefulWidget {
   State<FailureForm> createState() => _FailureFormState();
 }
 
+List<String> list = <String>[
+  "Was too hard",
+  "Didn't have time",
+  "Felt uncomfortable",
+  "Lacked motivation",
+  "Unforeseen circumstances",
+  "Other"
+];
+
 class _FailureFormState extends State<FailureForm> {
   final String _selectedReason = "";
   final String _writtenPart = "";
-  final bool _likedTask = false;
-
-  List<String> list = <String>[
-    "Was too hard",
-    "Didn't have time",
-    "Felt uncomfortable",
-  ];
+  bool _likedTask = false;
 
   @override
   Widget build(BuildContext context) {
@@ -28,52 +31,102 @@ class _FailureFormState extends State<FailureForm> {
     return Center(
       child: Form(
         key: formKey,
-        child: Column(
-          children: [
-            Padding(
-                padding: const EdgeInsets.fromLTRB(14, 30, 14, 0),
-                child: DropdownButton<String>(
-                  value: dropdownValue,
-                  icon: const Icon(Icons.arrow_downward),
-                  elevation: 16,
-                  style: const TextStyle(color: Colors.deepPurple),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.deepPurpleAccent,
-                  ),
-                  onChanged: (String? value) {
-                    setState(() {
-                      dropdownValue = value!;
-                    });
-                  },
-                  items: list.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                )),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(14, 100, 14, 0),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 51, 103, 151)),
-                child: Text(
-                  "Submit",
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(14, 30, 14, 20),
+            child: Column(
+              children: [
+                Text(
+                  "Why do you think todays tasks didn't go as planned?",
+                  textAlign: TextAlign.center,
                   style: GoogleFonts.comfortaa(
                     textStyle: Theme.of(context).textTheme.displayLarge,
-                    fontSize: 40,
+                    fontSize: 24,
                     fontWeight: FontWeight.w500,
                     color: Colors.white,
                   ),
                 ),
-              ),
-            )
-          ],
+                const SizedBox(height: 14),
+                const ReasonDropDown(),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 51, 103, 151)),
+                  child: Text(
+                    "Submit",
+                    style: GoogleFonts.comfortaa(
+                      textStyle: Theme.of(context).textTheme.displayLarge,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 300,
+                      child: Text(
+                        "Would you like to try this task again?",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.comfortaa(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Checkbox(
+                      value: _likedTask,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _likedTask = value ?? false;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class ReasonDropDown extends StatefulWidget {
+  const ReasonDropDown({super.key});
+
+  @override
+  State<ReasonDropDown> createState() => _ReasonDropDownState();
+}
+
+class _ReasonDropDownState extends State<ReasonDropDown> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        color: Colors.white,
+      ),
+      child: DropdownMenu<String>(
+        initialSelection: list.first,
+        onSelected: (String? value) {
+          setState(() {
+            dropdownValue = value!;
+          });
+        },
+        dropdownMenuEntries:
+            list.map<DropdownMenuEntry<String>>((String value) {
+          return DropdownMenuEntry<String>(value: value, label: value);
+        }).toList(),
       ),
     );
   }
